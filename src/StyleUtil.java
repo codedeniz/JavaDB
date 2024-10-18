253f15211033ed7f4165fc2385e2ab778b133281
Gokdeniz Akbuga <158981326+codedeniz@users.noreply.github.com>
2024-10-17 23:10:32 -0400

    Update StyleUtil.java

--- src/StyleUtil.java
+++ src/StyleUtil.java
@@ -2,7 +2,7 @@ import javafx.scene.Scene;
 
 public class StyleUtil {
     public static void applyStyle(Scene scene) {
-        // Add the CSS stylesheet
+       
         scene.getStylesheets().add(StyleUtil.class.getResource("/style.css").toExternalForm());
     }
 }
